0e2cf1e3a97b9c2a841ead47bde1d1f4177abfb2
Basmala-Abdullah <96146987+Basmala-Abdullah@users.noreply.github.com>
2022-03-16 13:20:11 +0200

    Add files via upload

---
+++ TheWatermelon/src/main/java/Watermelon.java
@@ -0,0 +1,9 @@
+public class Watermelon {
+    public String check_Watermelon(int w){
+        if (w%2 == 0 && w > 2 )
+            return "YES";
+        else
+            return "NO";
+
+    }
+}
---
+++ TheWatermelon/src/test/java/WatermelonTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WatermelonTest {
+
+    @Test
+    public void check_Watermelon() {
+        Watermelon test = new Watermelon();
+        assertEquals("YES", test.check_Watermelon(8));
+        assertEquals("NO", test.check_Watermelon(5));
+        assertEquals("YES", test.check_Watermelon(4));
+        assertEquals("NO", test.check_Watermelon(3));
+        assertEquals("NO", test.check_Watermelon(2));
+        assertEquals("NO", test.check_Watermelon(1));
+        assertEquals("NO", test.check_Watermelon(7));
+        assertEquals("YES", test.check_Watermelon(6));
+        assertEquals("YES", test.check_Watermelon(10));
+        assertEquals("NO", test.check_Watermelon(9));
+        assertEquals("NO", test.check_Watermelon(53));
+        assertEquals("NO", test.check_Watermelon(77));
+        assertEquals("YES", test.check_Watermelon(32));
+        assertEquals("YES", test.check_Watermelon(44));
+        assertEquals("YES", test.check_Watermelon(98));
+        assertEquals("NO", test.check_Watermelon(99));
+        assertEquals("YES", test.check_Watermelon(90));
+        assertEquals("NO", test.check_Watermelon(67));
+        assertEquals("YES", test.check_Watermelon(100));
+        assertEquals("YES", test.check_Watermelon(88));
+    }
+}
